6f50f3ff51e92827c501cf4326da7d63d77fb0fc
vee-jain <92858041+vee-jain@users.noreply.github.com>
2023-10-27 10:34:12 +0200

    Update test_RFunction.R
    Updated test that

--- tests/testthat/test_RFunction.R
+++ tests/testthat/test_RFunction.R
@@ -1,8 +1,8 @@
 library('move2')
 
 test_data <- test_data("input3_move2.rds") #file must be move2!
+actual <- rFunction(data = test_data, interval_option = "monthly")
 
 test_that("happy path", {
-  actual <- rFunction(data = test_data, interval_option = "monthly")
   expect_equal(test_data,actual)
 })
